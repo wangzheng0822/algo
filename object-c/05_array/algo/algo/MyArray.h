f8c9f0b6f5a2c557cf5196e9559719ef6f207d88
Wenru Dong <dongwenru.aaron.shenyang@gmail.com>
2018-10-03 22:19:11 +0100

    add implementations of array in python and objective-c

---
+++ object-c/05_array/algo/algo/MyArray.h
@@ -0,0 +1,13 @@
+//
+//  MyArray.h
+//  algo
+//
+//  Created by Wenru Dong on 2018/10/3.
+//  Copyright © 2018年 Wenru Dong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyArray : NSObject
+
+@end
---
+++ object-c/05_array/algo/algo/MyArray.m
@@ -0,0 +1,13 @@
+//
+//  MyArray.m
+//  algo
+//
+//  Created by Wenru Dong on 2018/10/3.
+//  Copyright © 2018年 Wenru Dong. All rights reserved.
+//
+
+#import "MyArray.h"
+
+@implementation MyArray
+
+@end
