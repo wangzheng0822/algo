a792e91a775fecc5d757980a8675901c9cd10145
jinshaohui <jinshaohui789@163.com>
2018-10-20 00:26:52 +0800

    add by jinshaohui for merge sentry

--- c-cpp/12_sorts/my12_sorts/merge_sort.c
+++ c-cpp/12_sorts/my12_sorts/merge_sort.c
@@ -10,6 +10,53 @@
 #include<string.h>
 #include<stdlib.h>
 
+#define SORT_MAX  (1000000)
+
+void dump(int a[],int size);
+void merge_sentry(int a[],int middle,int left,int right)
+{
+	int *pleft = NULL;
+	int *pright = NULL;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int left_size = middle - left + 1;
+	int right_size = right - middle;
+
+	
+	pleft = (int *)malloc(sizeof(int)*(left_size + 1));
+	assert(pleft != NULL);
+	pright = (int *)malloc(sizeof(int)*(right_size + 1));
+	assert(pright != NULL);
+
+	for(i = 0; i < left_size; i ++)
+	{
+		pleft[i] = a[left + i];
+	}
+	pleft[left_size] = SORT_MAX;
+	for(i = 0; i < right_size; i ++)
+	{
+		pright[i] = a[middle + 1 + i];
+	}
+	pright[right_size] = SORT_MAX;
+
+    for (k = left,i = 0,j = 0; k <= right; k++)
+	{
+		if (pleft[i] <= pright[j])
+		{
+			a[k] = pleft[i++];
+		}
+		else
+		{
+			a[k] = pright[j++];
+		}
+	}
+
+	free(pleft);
+	free(pright);
+
+    return;
+}
 
 /*两个有序数组合并*/
 void merge(int a[],int middle,int left,int right)
@@ -78,7 +125,7 @@ void merge_sort(int a[],int left,int right)
 	merge_sort(a,left,middle);
 	merge_sort(a,middle + 1,right);
 
-    merge(a,middle,left,right);
+    merge_sentry(a,middle,left,right);
 
 	return;
 }
@@ -86,6 +133,11 @@ void merge_sort(int a[],int left,int right)
 void dump(int a[],int size)
 {
 	int i = 0;
+
+	if(size == 0)
+	{
+		return;
+	}
 
 	printf("\r\n");
 	for (i = 0; i < size; i++ )
