b06c99632b7290783c1074e6fa6fa6c57de7cceb
yangchuz <joshua.zheng@hotmail.com>
2018-10-23 09:54:51 +0800

    Update Sorts.scala

--- scala/11_sorts/Sorts.scala
+++ scala/11_sorts/Sorts.scala
@@ -1,7 +1,7 @@
 import scala.util.control.Breaks._
 object Sorts {
   def main(args: Array[String]): Unit ={
-    println(bubbleSort(Array(0, 1, 2, 3, 4, 5, 6, 7)).mkString(", "))
+    println(bubbleSort(Array(0, 3, 7, 6, 4, 5, 1, 2)).mkString(", "))
   }
 
   def bubbleSort(arr: Array[Int]): Array[Int] = {
@@ -10,7 +10,7 @@ object Sorts {
       for(i <- (l-1) to (1, -1)){
         var flag = false
         for(j <- 0 until i){
-          if(arr(j) < arr(j+1)){
+          if(arr(j) > arr(j+1)){
             val tmp = arr(j)
             arr(j) = arr(j+1)
             arr(j+1) = tmp
