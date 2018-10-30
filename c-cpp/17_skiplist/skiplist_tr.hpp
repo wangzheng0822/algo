2fbe612975dcf86399d9178cfcca7aa4c42749a2
Liam Huang <liamhuang0205@gmail.com>
2018-10-30 18:30:29 +0800

    [cpp][17_skiplist] based on std::list, init.

---
+++ c-cpp/17_skiplist/skiplist_tr.hpp
@@ -0,0 +1,17 @@
+/**
+ * Created by Liam Huang (Liam0205) on 2018/10/30.
+ */
+
+#ifndef SKIPLIST_SKIPLIST_TR_HPP_
+#define SKIPLIST_SKIPLIST_TR_HPP_
+
+#include <functional>
+
+template <typename Value,
+          typename Compare = std::less<Value>>
+class skiplist {
+
+};
+
+#endif  // SKIPLIST_SKIPLIST_TR_HPP_
+
