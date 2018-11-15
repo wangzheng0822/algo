ccf862c51947463f594abdc73d3fd4884cf7e771
leotyliu(刘天一) <leotyliu@tencent.com>
2018-11-15 16:07:29 +0800

    BST by golang

---
+++ go/24_tree/BinaryTree_test.go
@@ -0,0 +1,30 @@
+package _4_tree
+
+import "testing"
+
+func TestBinaryTree_InOrderTraverse(t *testing.T) {
+	binaryTree := NewBinaryTree(1)
+	binaryTree.root.left = NewNode(3)
+	binaryTree.root.right = NewNode(4)
+	binaryTree.root.right.left = NewNode(5)
+
+	binaryTree.InOrderTraverse()
+}
+
+func TestBinaryTree_PreOrderTraverse(t *testing.T) {
+	binaryTree := NewBinaryTree(1)
+	binaryTree.root.left = NewNode(3)
+	binaryTree.root.right = NewNode(4)
+	binaryTree.root.right.left = NewNode(5)
+
+	binaryTree.PreOrderTraverse()
+}
+
+func TestBinaryTree_PostOrderTraverse(t *testing.T) {
+	binaryTree := NewBinaryTree(1)
+	binaryTree.root.left = NewNode(3)
+	binaryTree.root.right = NewNode(4)
+	binaryTree.root.right.left = NewNode(5)
+
+	binaryTree.PostOrderTraverse()
+}
