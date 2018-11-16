f1897d10291d30e4fa015ab50802de2eca7b1b75
lee <554050334@qq.com>
2018-11-16 18:40:50 +0800

    add a new implemention way for BT post order traverse

--- go/24_tree/BinaryTree_test.go
+++ go/24_tree/BinaryTree_test.go
@@ -28,3 +28,12 @@ func TestBinaryTree_PostOrderTraverse(t *testing.T) {
 
 	binaryTree.PostOrderTraverse()
 }
+
+func TestBinaryTree_PostOrderTraverse2(t *testing.T) {
+	binaryTree := NewBinaryTree(1)
+	binaryTree.root.left = NewNode(3)
+	binaryTree.root.right = NewNode(4)
+	binaryTree.root.right.left = NewNode(5)
+
+	binaryTree.PostOrderTraverse2()
+}
