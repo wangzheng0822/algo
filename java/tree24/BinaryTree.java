f89daff4db124cda74d06b022ac9ad5f87f59299
mambo-wang <mambo1991@163.com>
2019-01-23 17:31:25 +0800

    二叉树的遍历

--- java/tree24/BinaryTree.java
+++ java/tree24/BinaryTree.java
@@ -4,83 +4,60 @@ public class BinaryTree {
 
     public static void main(String[] args) {
 
-        BinaryTreeNode root = new BinaryTreeNode(1, new BinaryTreeNode(2, new BinaryTreeNode(4), new BinaryTreeNode(5)),
-                new BinaryTreeNode(3, new BinaryTreeNode(6), new BinaryTreeNode(7)));
+        Node root = new Node(1, new Node(2, new Node(4), new Node(5)),
+                new Node(3, new Node(6), new Node(7)));
         System.out.println(
                 "----------1-------\n" +
                 "----2--------3----\n" +
-                        "-4-----5- -6------7--");
+                "-4-----5- -6------7--\n");
+
         preOrder(root);
-        System.out.println("--------------------");
+        System.out.println("\n--------------------");
         inOrder(root);
-        System.out.println("--------------------");
+        System.out.println("\n--------------------");
         postOrder(root);
     }
 
 
-    static void preOrder(BinaryTreeNode root) {
+    static void preOrder(Node root) {
         if (root == null) return;
-        System.out.println(root.data);
+        System.out.print(root.data);
         preOrder(root.left);
         preOrder(root.right);
     }
 
-    static void inOrder(BinaryTreeNode root) {
+    static void inOrder(Node root) {
         if (root == null) return;
         inOrder(root.left);
-        System.out.println(root.data);
+        System.out.print(root.data);
         inOrder(root.right);
     }
 
-    static void postOrder(BinaryTreeNode root) {
+    static void postOrder(Node root) {
         if (root == null) return;
         postOrder(root.left);
         postOrder(root.right);
-        System.out.println(root.data);
+        System.out.print(root.data);
     }
 
     //二叉树节点
-    static class BinaryTreeNode {
+    static class Node {
         private int data;
-        private BinaryTreeNode left;
-        private BinaryTreeNode right;
+        private Node left;
+        private Node right;
 
-        public BinaryTreeNode() {}
+        public Node() {}
 
-        public BinaryTreeNode(int data, BinaryTreeNode left, BinaryTreeNode right) {
+        public Node(int data, Node left, Node right) {
             super();
             this.data = data;
             this.left = left;
             this.right = right;
         }
 
-        public BinaryTreeNode(int data) {
+        public Node(int data) {
             super();
             this.data = data;
-        }
-
-        public int getData() {
-            return data;
-        }
-
-        public void setData(int data) {
-            this.data = data;
-        }
-
-        public BinaryTreeNode getLeft() {
-            return left;
-        }
-
-        public void setLeft(BinaryTreeNode left) {
-            this.left = left;
-        }
-
-        public BinaryTreeNode getRight() {
-            return right;
-        }
-
-        public void setRight(BinaryTreeNode right) {
-            this.right = right;
         }
     }
 }
