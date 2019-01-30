f6f6d53b7836d212562692c568be0760c1550e49
Ivan Li <email2liyang@gmail.com>
2019-01-30 22:03:37 +0800

    DFS topology sort

--- scala/src/main/scala/ch43_topology_sort/GraphTopology.scala
+++ scala/src/main/scala/ch43_topology_sort/GraphTopology.scala
@@ -1,6 +1,7 @@
 package ch43_topology_sort
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 class GraphTopology(vertex: Int) {
 
@@ -16,7 +17,7 @@ class GraphTopology(vertex: Int) {
 
   def topologySortByKahn(): Array[Int] = {
     val seq = new mutable.ArrayBuffer[Int]()
-    //inDgrees contains all the inDegree for a given node
+    //inDegrees contains all the inDegree for a given node
     val inDegrees = new Array[Int](vertex)
     for (i <- Range(0, vertex)) {
       for (j <- adjacency(i).indices) {
@@ -52,6 +53,47 @@ class GraphTopology(vertex: Int) {
     }
 
     seq.toArray
+  }
+
+  def topologySortByDFS(): Array[Int] = {
+    val inverseAdj = new Array[mutable.MutableList[Int]](vertex)
+    for (i <- Range(0, vertex)) {
+      inverseAdj(i) = new mutable.MutableList[Int]()
+    }
+
+    //build the inverse adj
+    for (i <- Range(0, vertex)) {
+      for (j <- adjacency(i).indices) {
+        val index = adjacency(i).get(j).get
+        inverseAdj(index) += i
+      }
+    }
+
+    val visited = new Array[Boolean](vertex)
+    val seq = new ArrayBuffer[Int]()
+    for (i <- Range(0, vertex)) {
+      if (!visited(i)) {
+        visited(i) = true
+        //call dfs
+        seq ++= dfs(i, inverseAdj, visited)
+      }
+    }
+
+    seq.toArray
+  }
+
+  def dfs(index: Int, inverseAdj: Array[mutable.MutableList[Int]], visited: Array[Boolean]): ArrayBuffer[Int] = {
+    val seq = new ArrayBuffer[Int]()
+
+    for (i <- inverseAdj(index).indices) {
+      val sourceIndex = inverseAdj(index).get(i).get
+      if (!visited(sourceIndex)) {
+        visited(sourceIndex) = true
+        seq ++= dfs(sourceIndex, inverseAdj, visited)
+      }
+    }
+    seq += index
+    seq
   }
 }
 
