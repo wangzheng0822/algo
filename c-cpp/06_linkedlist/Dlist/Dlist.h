170bf09421a6b2f2f9dfbe58fc20fb273f1673fd
Devil0F <40351237+Devil0F@users.noreply.github.com>
2019-03-08 14:56:35 +0800

    链表
    添加注释，方便个人理解

--- c-cpp/06_linkedlist/Dlist/Dlist.h
+++ c-cpp/06_linkedlist/Dlist/Dlist.h
@@ -7,17 +7,17 @@
  ************************************************************************/
 #include<stdio.h>
 
-typedef struct DlistNode
-{
-	struct DlistNode *prev;
-	struct DlistNode *next;
-	int    data;
+typedef struct DlistNode          //双向链表中每一个节点
+{				  	
+	struct DlistNode *prev;   //节点前项指针
+	struct DlistNode *next;   //节点后项指针
+	int    data;              //数据
 }stDlistNode;
 
-typedef struct Dlisthead
+typedef struct Dlisthead          //定义链表总体
 {
-	int size;
-	stDlistNode *head;
-	stDlistNode *tail;
+	int size;                 //链表长度
+	stDlistNode *head;        //头指针
+	stDlistNode *tail;        //尾部指针
 }stDlistHead;
 
