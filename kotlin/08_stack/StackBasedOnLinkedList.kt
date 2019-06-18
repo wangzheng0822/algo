cb1e9c1a8a1de8c6be7b41027db3ada16070d691
Zackratos <869649338@qq.com>
2019-06-19 01:27:35 +0800

    update code format

--- kotlin/08_stack/StackBasedOnLinkedList.kt
+++ kotlin/08_stack/StackBasedOnLinkedList.kt
@@ -5,32 +5,32 @@
  */
 
 class StackBasedOnLinkedList {
+    
+    private var top: Node? = null
 
-	private var top: Node? = null
+    fun push(value: Int) {
+        val newNode = Node(value, null)
+        // 不管 top 是不是 null，都可以这么写
+        newNode.next = top
+        top = newNode
+    }
 
-	fun push(value: Int) {
-		val newNode = Node(value, null)
-		// 不管 top 是不是 null，都可以这么写
-		newNode.next = top
-		top = newNode
-	}
+    fun pop(): Int {
+        if (top == null) return -1
+        val node = top
+        top = top!!.next
+        return node!!.data
+    }
 
-	fun pop(): Int {
-		if (top == null) return -1
-		val node = top
-		top = top!!.next
-		return node!!.data
-	}
-
-	fun printAll() {
-		var p = top
-		while(p != null) {
-			print("${p.data} ")
-			p = p.next
-		}
-		println()
-	}
+    fun printAll() {
+        var p = top
+        while (p != null) {
+            print("${p.data} ")
+            p = p.next
+        }
+        println()
+    }
 
     class Node(var data: Int, var next: Node?)
-    
+
 }
