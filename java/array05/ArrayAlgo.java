aa69b3441d228a40adbda18834af5b91066ee1fd
迎风 <mambo1991@163.com>
2019-11-26 08:49:20 +0800

    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。

--- java/array05/ArrayAlgo.java
+++ java/array05/ArrayAlgo.java
@@ -2,6 +2,27 @@ package array05;
 
 public class ArrayAlgo {
 
+    /**
+     * 时间 11.25
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     */
+    public static int[] twoSum(int[] nums, int target) throws Exception {
+
+        for (int i = 0; i < nums.length; i ++) {
+
+            for (int j = i + 1; j < nums.length; j ++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new Exception("没有符合条件的两个元素");
+    }
 
     /**
      * 时间 11.23
