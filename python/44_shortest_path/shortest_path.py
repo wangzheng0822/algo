8f47ac2c58cabeaab8180c0a4db6bc89a6feded1
AllenGFLiu <allengfliu@foxmail.com>
2020-01-10 17:03:07 +0800

    為Dijkstra算法中用到的PriorityQueue增加update動作

--- python/44_shortest_path/shortest_path.py
+++ python/44_shortest_path/shortest_path.py
@@ -19,33 +19,51 @@ class Vertex:
     vertex_id: int
 
 class Graph:
+
+    REMOVED = '<removed-task>'
+
     def __init__(self, num_vertices: int):
         self._num_vertices = num_vertices
         self._adjacency = [[] for _ in range(num_vertices)]
+        self._entry_finder = {}
     
     def add_edge(self, from_vertex: int, to_vertex: int, weight: int) -> None:
         self._adjacency[from_vertex].append(Edge(from_vertex, to_vertex, weight))
+
+    def update_priorityqueue(self, vertex_id):
+        poped_entry = self._entry_finder.pop(vertex_id)
+        poped_entry[-1] = Graph.REMOVED
 
     def dijkstra(self, from_vertex: int, to_vertex: int) -> None:
         vertices = [Vertex(i) for i in range(self._num_vertices)]
         vertices[from_vertex].distance_to_start = 0
         visited = [False] * self._num_vertices
         predecessor = [-1] * self._num_vertices
+        
         q = PriorityQueue()
-        q.put(vertices[from_vertex])
+        entry = [vertices[from_vertex].distance_to_start, vertices[from_vertex]]
+        q.put(entry)
+        self._entry_finder[from_vertex] = entry
+
         visited[from_vertex] = True
         while not q.empty():
-            min_vertex = q.get()
-            if min_vertex.vertex_id == to_vertex:
-                break
-            for edge in self._adjacency[min_vertex.vertex_id]:
-                next_vertex = vertices[edge.end_id]
-                if min_vertex.distance_to_start + edge.weight < next_vertex.distance_to_start:
-                    next_vertex.distance_to_start = min_vertex.distance_to_start + edge.weight
-                    predecessor[next_vertex.vertex_id] = min_vertex.vertex_id
-                    if not visited[next_vertex.vertex_id]:
-                        q.put(next_vertex)
-                        visited[next_vertex.vertex_id] = True
+            min_vertex = q.get()[1]
+            if min_vertex is not Graph.REMOVED:
+                del self._entry_finder[min_vertex.vertex_id]
+                if min_vertex.vertex_id == to_vertex:
+                    break
+                for edge in self._adjacency[min_vertex.vertex_id]:
+                    next_vertex = vertices[edge.end_id]
+                    if min_vertex.distance_to_start + edge.weight < next_vertex.distance_to_start:
+                        next_vertex.distance_to_start = min_vertex.distance_to_start + edge.weight
+                        predecessor[next_vertex.vertex_id] = min_vertex.vertex_id
+                        entry = [next_vertex.distance_to_start, next_vertex]
+                        if not visited[next_vertex.vertex_id]:
+                            visited[next_vertex.vertex_id] = True
+                        else:
+                            self.update_priorityqueue(next_vertex.vertex_id)
+                        q.put(entry)
+                        self._entry_finder[next_vertex.vertex_id] = entry
             
         path = lambda x: path(predecessor[x]) + [str(x)] if from_vertex != x else [str(from_vertex)]
         print("->".join(path(to_vertex)))
